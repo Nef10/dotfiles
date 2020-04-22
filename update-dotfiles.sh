d09673975c25fa4c4c5bfb5193e6ae34344e8e02
Steffen Kötte <steffen.koette@gmail.com>
2020-04-21 23:10:18 -0700

    Adjust output of update script
    - Move arrows printing to functions like in other files
    - Adjust arrow output
    - prepend arrows to git output

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -2,22 +2,41 @@
 
 main() {
     UPDATE=1
-    print -P "%F{blue}=> Checking for updates...%f"
+    step "Checking for updates of dotfiles repository"
     git -C $HOME/.dotfiles fetch &> /dev/null
     if [ $(git -C $HOME/.dotfiles rev-parse HEAD) '==' $(git -C $HOME/.dotfiles rev-parse @{u}) ]; then
-        print -P "%F{green}===> Already up to date.%f"
+        success "Already up to date"
         UPDATE=0
         if [[ "$1" = "--force" ]]; then
-            print -P "%F{yellow}===> Forcing update needed%f"
+            warning "Forcing update"
             UPDATE=1
         fi
     fi
     if [[ "$UPDATE" -eq 1 ]]; then
-        print -P "%F{white}===> Update needed%f"
-        print -P "%F{blue}=> Updating...%f"
-        git -C $HOME/.dotfiles pull # pull first, so the script is updated before executing
+        warning "Update needed"
+        step "Updating..."
+        git -C $HOME/.dotfiles pull | prependInfo # pull first, so the script is updated before executing
         $HOME/.dotfiles/setup-mac-os.sh --no-pull
     fi
+}
+
+function step() {
+    print -P "%F{blue}=> $1%f"
+}
+
+function warning() {
+    print -P "%F{yellow}==> $1%f"
+}
+
+function success() {
+    print -P "%F{green}==> $1%f"
+}
+
+function prependInfo() {
+    while read line;
+    do
+        print -P "%F{white}===> $line%f";
+    done;
 }
 
 main "$@"
