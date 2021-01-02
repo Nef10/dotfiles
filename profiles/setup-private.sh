d6b51d23453875547b368461d5ea4a179571a72f
Steffen Kötte <steffen.koette@gmail.com>
2021-01-02 15:52:56 -0800

    Improve repo automation
    Do not use pager do not require user to press key
    Skip color config step by pretenting to not be a shell

--- profiles/setup-private.sh
+++ profiles/setup-private.sh
@@ -8,12 +8,12 @@ else
     mkdir "${PROJECTS_DIR}"
     success "Project folder created"
     step "Initializing repo"
-    (cd "${PROJECTS_DIR}"; repo init -u git@github.com:Nef10/repo-manifest.git)
+    (cd "${PROJECTS_DIR}"; repo init -u git@github.com:Nef10/repo-manifest.git | cat)
     success "Initialized repo"
     step "Downloading repositories"
-    (cd "${PROJECTS_DIR}"; repo sync)
+    (cd "${PROJECTS_DIR}"; repo --no-pager sync | cat)
     success "Downloaded repositories"
     step "Checking out branches and downloading git lfs files"
-    (cd "${PROJECTS_DIR}"; repo forall -p -c 'git checkout $REPO_RREV && git lfs pull')
+    (cd "${PROJECTS_DIR}"; repo --no-pager forall -p -c 'git checkout $REPO_RREV && git lfs pull' | cat)
     success "Checked out branches and downloaded git lfs files"
 fi
