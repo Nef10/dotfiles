51c808506ef84616c5bdfee60ad4f6f149610948
Steffen Kötte <steffen.koette@gmail.com>
2021-10-24 21:50:19 -0700

    Move SSH Key setup before project download

--- profiles/setup-private.sh
+++ profiles/setup-private.sh
@@ -3,6 +3,11 @@
 step "Updating KeepingYouAwake settings"
 zsh ${DOTFILES_REPO}/settings/settings.sh $DOTFILES_REPO/settings/keeping_you_awake.csv set
 
+step "Setup SSH Keys"
+install_ssh_key "id_rsa"
+install_ssh_key "id_rsa_ci"
+install_ssh_key "id_rsa_ghg"
+
 step "Setup Project folder"
 PROJECTS_DIR="$HOME/Projects"
 if [[ -d "${PROJECTS_DIR}" ]]; then
@@ -20,8 +25,3 @@ else
     (cd "${PROJECTS_DIR}"; repo --no-pager forall -p -c 'git checkout $REPO_RREV && git lfs pull' | cat)
     success "Checked out branches and downloaded git lfs files"
 fi
-
-step "Setup SSH Keys"
-install_ssh_key "id_rsa"
-install_ssh_key "id_rsa_ci"
-install_ssh_key "id_rsa_ghg"
